296f4d781cd87f6ff07941a9cf33c61fd3a811db
osama zia <osamazia13@gmal.com>
2021-01-27 18:42:52 +0500

    First Commit

---
+++ main.py
@@ -0,0 +1,30 @@
+import requests
+
+x = requests.get('https://webscraper.io/test-sites')
+
+res = x.text
+
+file = open("copy.txt", "w")
+count = 0
+for line in res:
+    for character in line:
+        if character == '<':
+            count = 1
+            continue
+        elif character == '>':
+            count = 0
+            continue
+        elif count == 1:
+            continue
+        else:
+            file.write(character)
+
+file.close()
+
+file = open("copy.txt", "r")
+lines = file.readlines()
+file.close()
+lines = filter(lambda x: not x.isspace(), lines)
+file = open("copy.txt", "w")
+file.writelines(lines)
+file.close()
